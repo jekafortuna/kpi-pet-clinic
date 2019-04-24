22796f3c038966e05b39b3379f176351d408948b
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2019-04-24 14:58:15 +0300

    Initial Project Add

---
+++ src/main/java/ua/kpi/kpipetclinic/KpiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.kpipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpiPetClinicApplication.class, args);
+    }
+
+}
