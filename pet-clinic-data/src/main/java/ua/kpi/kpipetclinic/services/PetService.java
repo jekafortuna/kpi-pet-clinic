a4b5491e192741507145e8b7dd23f5b1a3650493
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2019-05-13 16:27:55 +0300

    Implement Interfaces for Services. Closes #5

---
+++ pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package ua.kpi.kpipetclinic.services;
+
+import ua.kpi.kpipetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
