f59c050c99422b8489571cab1a53e5e1096f98fa
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2019-05-14 12:59:26 +0300

    Adding base entity. Closes #10

--- pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package ua.kpi.kpipetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package ua.kpi.kpipetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
