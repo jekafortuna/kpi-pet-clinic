bb949df5499e407ddf119d62fac6656d269a6847
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2019-05-15 13:49:32 +0300

    Refactoring to Common CRUD Interface. Closes #11

---
+++ pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/services/CrudService.java
@@ -0,0 +1,16 @@
+package ua.kpi.kpipetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
