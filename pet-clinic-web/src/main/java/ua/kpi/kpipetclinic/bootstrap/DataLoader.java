571361656e01a53a65d8854062b15e37a73b4dbe
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2019-05-25 15:59:01 +0300

    Adding Bootstrap for Data. Closes #14

--- pet-clinic-web/src/main/java/ua/kpi/kpipetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ua/kpi/kpipetclinic/bootstrap/DataLoader.java
@@ -3,6 +3,7 @@ package ua.kpi.kpipetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ua.kpi.kpipetclinic.model.Owner;
+import ua.kpi.kpipetclinic.model.Vet;
 import ua.kpi.kpipetclinic.services.OwnerService;
 import ua.kpi.kpipetclinic.services.VetService;
 import ua.kpi.kpipetclinic.services.map.OwnerServiceMap;
@@ -27,5 +28,30 @@ public class DataLoader implements CommandLineRunner {
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glennane");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded owners...");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Alex");
+        vet1.setLastName("Black");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("John");
+        vet2.setLastName("Brown");
+
+        vetService.save(vet2);
+
+        System.out.println("Loaded vets...");
     }
 }
