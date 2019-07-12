a18cccfd5b0e487c56ac11154a71260404840487
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2019-07-12 15:04:41 +0300

    Created Speciality Map Service implementation. Closes #29

---
+++ pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package ua.kpi.kpipetclinic.services;
+
+import ua.kpi.kpipetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+}
---
+++ pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package ua.kpi.kpipetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import ua.kpi.kpipetclinic.model.Speciality;
+import ua.kpi.kpipetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
