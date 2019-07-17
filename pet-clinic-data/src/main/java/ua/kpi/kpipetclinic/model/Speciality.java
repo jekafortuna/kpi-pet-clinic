375a289a65518f1b54dc4b6b8e6462e3eed51a51
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2019-07-17 18:30:17 +0300

    Updated Owners and Vets to use JPA Entity mapping. Closes #34

--- pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/ua/kpi/kpipetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package ua.kpi.kpipetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
