f314c6e90c6ea2545ce9d4eac6df4de05c8e7579
Yevhenii Fortuna <evgeniy.fortuna@gmail.com>
2019-08-17 12:59:49 +0300

    Added JSON endpoint to Vets. Closes #67

--- pet-clinic-web/src/main/java/ua/kpi/kpipetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ua/kpi/kpipetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package ua.kpi.kpipetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ua.kpi.kpipetclinic.model.Vet;
 import ua.kpi.kpipetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +25,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
